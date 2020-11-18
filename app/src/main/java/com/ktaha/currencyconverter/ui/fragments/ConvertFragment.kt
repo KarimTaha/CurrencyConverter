745164bbc29548ea201eed69891a3a76c0342084
karimTaha <karim.taha@oracle.com>
2020-11-18 17:08:41 +0200

    Add layouts structure, fragments classes

---
+++ app/src/main/java/com/ktaha/currencyconverter/ui/fragments/ConvertFragment.kt
@@ -0,0 +1,12 @@
+package com.ktaha.currencyconverter.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ktaha.currencyconverter.R
+
+class ConvertFragment : Fragment(R.layout.fragment_convert) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/ktaha/currencyconverter/ui/fragments/ListCurrenciesFragment.kt
@@ -0,0 +1,12 @@
+package com.ktaha.currencyconverter.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ktaha.currencyconverter.R
+
+class ListCurrenciesFragment : Fragment(R.layout.fragment_list_currencies){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

7e59e836aec03cae38d62b746973407ccee7bcc2
karimTaha <karim.taha@oracle.com>
2020-11-20 18:54:43 +0200

    Add logic to convert between two currencies

--- app/src/main/java/com/ktaha/currencyconverter/ui/CurrencyViewModel.kt
+++ app/src/main/java/com/ktaha/currencyconverter/ui/CurrencyViewModel.kt
@@ -1,11 +1,10 @@
 package com.ktaha.currencyconverter.ui
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.ktaha.currencyconverter.models.Currency
 import com.ktaha.currencyconverter.models.CurrencyResponse
 import com.ktaha.currencyconverter.repository.CurrencyRepository
 import com.ktaha.currencyconverter.util.Resource
@@ -16,14 +15,14 @@ class CurrencyViewModel(
         app: Application,
         private val currencyRepository: CurrencyRepository
 ): AndroidViewModel(app) {
-    var currenciesArray = arrayListOf<Currency>()
     val currencies: MutableLiveData<Resource<CurrencyResponse>> = MutableLiveData()
+    val TAG = "CurrencyViewModel"
 
     init {
         getCurrencies()
     }
 
-    fun getCurrencies() = viewModelScope.launch {
+    private fun getCurrencies() = viewModelScope.launch {
         val response = currencyRepository.getCurrencies()
         currencies.postValue(handleCurrencyResponse(response))
     }
@@ -35,5 +34,37 @@ class CurrencyViewModel(
             }
         }
         return Resource.Error(response.message())
+    }
+
+    fun convertCurrency(amount: Double, sourceCurrency: String, targetCurrency: String):Double {
+        Log.d(TAG, "Converting amount = $amount from $sourceCurrency to $targetCurrency")
+        if (sourceCurrency == targetCurrency) {
+            return amount
+        }
+        else {
+            val response = currencies.value
+            if (response is Resource.Success) {
+                response.data?.let { currenciesResponse->
+                    val ratesMap = currenciesResponse.rates.getCurrenciesMap()
+                    if (!ratesMap.containsKey(sourceCurrency) || !ratesMap.containsKey(targetCurrency)) {
+                        return -1.0
+                    } else {
+                        return when {
+                            sourceCurrency == "USD" -> {
+                                amount * (ratesMap[targetCurrency] ?: 1.0)
+                            }
+                            targetCurrency == "USD" -> {
+                                amount / (ratesMap[sourceCurrency] ?: 1.0)
+                            }
+                            else -> {
+                                val valueInUSD = amount / (ratesMap[sourceCurrency] ?: 1.0)
+                                valueInUSD * (ratesMap[targetCurrency] ?: 1.0)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return -1.0
     }
 }
--- app/src/main/java/com/ktaha/currencyconverter/ui/fragments/ConvertFragment.kt
+++ app/src/main/java/com/ktaha/currencyconverter/ui/fragments/ConvertFragment.kt
@@ -1,12 +1,46 @@
 package com.ktaha.currencyconverter.ui.fragments
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.ktaha.currencyconverter.R
+import com.ktaha.currencyconverter.ui.CurrencyViewModel
+import com.ktaha.currencyconverter.ui.MainActivity
+import kotlinx.android.synthetic.main.fragment_convert.*
 
 class ConvertFragment : Fragment(R.layout.fragment_convert) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var viewModel: CurrencyViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        
+        etSourceAmount.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_UP && keyCode != KeyEvent.KEYCODE_BACK) {
+                if (etSourceAmount.text.isNotEmpty()) {
+                    val sourceAmount = etSourceAmount.text.toString().toDouble()
+                    etTargetAmount.setText((viewModel.convertCurrency(sourceAmount, tvSourceCurrency.text.toString(), tvTargetCurrency.text.toString())).toString())
+                }
+                else {
+                    etTargetAmount.setText("")
+                }
+            }
+            false
+        }
+
+        etTargetAmount.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_UP && keyCode != KeyEvent.KEYCODE_BACK) {
+                if (etTargetAmount.text.isNotEmpty()) {
+                    val targetAmount = etTargetAmount.text.toString().toDouble()
+                    etSourceAmount.setText((viewModel.convertCurrency(targetAmount, tvTargetCurrency.text.toString(), tvSourceCurrency.text.toString())).toString())
+                }
+                else {
+                    etSourceAmount.setText("")
+                }
+            }
+            false
+        }
     }
 }
